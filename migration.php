4889e68b74a3254a2b49746dbc102cc7036b238b
tomkop1369 <tomaszkopacz44@gmail.com>
2020-09-02 11:03:25 +0000

    Skrypt migration.php do poprawy

--- migration.php
+++ migration.php
@@ -2,28 +2,15 @@
 require_once('zmienne.php');
 
 $mysqli = new mysqli($host, $username, $password, $database);
-//$query = 'SELECT * FROM Użytkownik';
-
-//	if (isset($query)){
-//		echo $query;
-//		}
-//	else {
-
-$table = " CREATE TABLE Użytkownik (
-ID INT(6) NOT NULL auto_increment,
-Imię VARCHAR(45) NOT NULL,
-Nazwisko VARCHAR(45) NOT NULL,
-Login VARCHAR(45) NOT NULL,
-Hasło VARCHAR(45) NOT NULL,
-Nazwa_firmy VARCHAR(45) NOT NULL,
-NIP INT(45) NOT NULL,
-Rodzaj_użytkownika ENUM('Właściciel', 'Pracownik', 'Użytkownik') NOT NULL,
-E_mail VARCHAR(45) NOT NULL,
-Fax VARCHAR(45)NOT NULL,
-PRIMARY KEY (ID), UNIQUE id (ID), KEY id_2 (ID))";
+$table =" CREATE TABLE User ( Id int(6) not null auto_increment, Imie varchar(45) not null, Nazwisko varchar(45) not null, Login varchar(45) not null, Hasło varchar(45) not null, Nazwa_firmy varchar(45)not null, NIP int(13) not null, Rodzaj_uzytkownika enum('Właściciel', 'Pracownik', 'Użytkownik') not null, E_mail varchar(45) not null, Fax varchar(45) not null, PRIMARY KEY (ID), UNIQUE id (ID))";
 
 $result = $mysqli->query($table);
-//echo $query;
-//}
+if($result){ 
+echo  'utworzono bazę danych';
+	} 
+else {
+ echo  '    nie utworzono bazy danych';
+}
+
 ?>
 
