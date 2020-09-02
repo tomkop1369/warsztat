78eed4610dc73c049aa2419a07b46d1975b97dee
tomkop1369 <tomaszkopacz44@gmail.com>
2020-09-02 14:32:57 +0000

    skrypt seed.php

--- seed.php
+++ seed.php
@@ -2,7 +2,8 @@
 require_once('zmienne.php');
 $mysqli = new mysqli($host, $username, $password, $database);
 
-$query =" INSERT INTO Users (Id, Imię, Nazwisko, Login, Hasło, Nazwa_firmy, NIP, Rodzaj_użytkownika, E_mail, Fax) values('1', 'Jan', 'Kowalski', 'Jan.kow', 'Jankow', 'Janpol', '231424131', 'Właściciel', 'Jan.kow@wp.pl' '2132132131')";
+$query =" INSERT INTO Users (Imie, Nazwisko, Login, Hasło, Nazwa_firmy, NIP, Rodzaj_uzytkownika, E_mail, Fax) VALUES ('Jan', 'Kowalski', 'Janko', 'Janko123', 'Janpol', '213242413', 'Właściciel', 'Jan.kow@wp.pl', 'janfax123')";
+
 $result = $mysqli->query($query);
 if($result){ 
 echo  'utworzono bazę danych';
@@ -10,5 +11,5 @@ echo  'utworzono bazę danych';
 else {
  echo  '    nie utworzono bazy danych';
 }
-
+$mysqli->close();
 ?> 
