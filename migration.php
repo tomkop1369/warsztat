822a662a71f1dd4b64a80d41eeb9f4b45f42339b
tomkop1369 <tomaszkopacz44@gmail.com>
2020-09-03 10:02:21 +0000

    nowe migration.php

--- migration.php
+++ migration.php
@@ -2,8 +2,9 @@
 require_once('zmienne.php');
 
 $mysqli = new mysqli($host, $username, $password, $database);
-$table =" CREATE TABLE User ( Id int(6) not null auto_increment, Imie varchar(45) not null, Nazwisko varchar(45) not null, Login varchar(45) not null, Hasło varchar(45) not null, Nazwa_firmy varchar(45)not null, NIP int(13) not null, Rodzaj_uzytkownika enum('Właściciel', 'Pracownik', 'Użytkownik') not null, E_mail varchar(45) not null, Fax varchar(45) not null, PRIMARY KEY (ID), UNIQUE id (ID))";
-
+$drop = " DROP TABLE IF EXISTS `Users`;";
+$table =" CREATE TABLE Users ( Id int(6) not null auto_increment, Imie varchar(45) not null, Nazwisko varchar(45) not null, Login varchar(45) not null, Hasło varchar(45) not null, Nazwa_firmy varchar(45)not null, NIP int(13) not null, Rodzaj_uzytkownika enum('Właściciel', 'Pracownik', 'Użytkownik') not null, E_mail varchar(45) not null, Fax varchar(45) not null, PRIMARY KEY (ID), UNIQUE id (ID));";
+$wynik = $mysqli->query($drop);
 $result = $mysqli->query($table);
 if($result){ 
 echo  'utworzono bazę danych';
