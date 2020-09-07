5e4749981e3813550bc0991b6fc44a30840dd888
tomkop1369 <tomaszkopacz44@gmail.com>
2020-09-07 12:42:06 +0000

    Dodany hash do hasła

--- seed.php
+++ seed.php
@@ -1,8 +1,9 @@
 <?php
 require_once('zmienne.php');
 $mysqli = new mysqli($host, $username, $password, $database);
-
-$query =" INSERT INTO Users (Imie, Nazwisko, Login, Hasło, Nazwa_firmy, NIP, Rodzaj_uzytkownika, E_mail, Fax) VALUES ('Jan', 'Kowalski', 'Janko', 'Janko123', 'Janpol', '213242413', 'Właściciel', 'Jan.kow@wp.pl', 'janfax123')";
+$a = 'Janko123';
+$hash = sha1($a);
+$query =" INSERT INTO Users (Imie, Nazwisko, Login, Haslo, Nazwa_firmy, NIP, Rodzaj_uzytkownika, E_mail, Fax) VALUES ('Jan', 'Kowalski', 'Janko', '$hash', 'Janpol', '213242413', 'Właściciel', 'Jan.kow@wp.pl', 'janfax123')";
 
 $result = $mysqli->query($query);
 if($result){ 
